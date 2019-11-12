84b54e2a9857a144a675941f051acb5755eb2898
Demianzx <demianz999@gmail.com>
2019-11-12 15:25:58 -0600

    cambios menores

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react';
-import {Navbar,Nav,NavDropdown,Form,Button,FormControl,Image} from 'react-bootstrap'
+import {Navbar,Nav,NavDropdown,Form,Button,FormControl} from 'react-bootstrap'
 
 
 import './Header.css'
